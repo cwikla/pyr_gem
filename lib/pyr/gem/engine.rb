26be48996e80203998e88fa1b32b3e33af0a9460
John Cwikla <cwikla@fwix.com>
2016-03-29 14:03:30 -0700

    Version geminabox

--- lib/pyr/gem/engine.rb
+++ lib/pyr/gem/engine.rb
@@ -1,7 +1,7 @@
 module Pyr
   module Gem
     class Engine < ::Rails::Engine
-      config.gem_server_url = "http://pyr:akbash42/gems.cwikla.com"
+      config.gem_server_url = "http://pyr:akbash42@gems.cwikla.com"
     end
   end
 end
